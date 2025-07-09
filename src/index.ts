25a6942a9c27c04453bdbd70a0ff43fdff9a9f3a
laurhakob <laurhakob@gmail.com>
2025-07-09 04:20:10 +0400

    setup bunny

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
 export * from "./middleware";
 
+export const BUNNY = {
+    STREAM_BASE_URL: "https://video.bunnycdn.com/library",
+    STORAGE_BASE_URL: "https://storage.bunnycdn.com/jsm-snapcast-recording",
+    CDN_URL: "https://jsm-snapcast-recording.b-cdn.net",
+    EMBED_URL: "https://iframe.mediadelivery.net/embed",
+    TRANSCRIPT_URL: "https://vz-759d59b0-270.b-cdn.net",
+};
